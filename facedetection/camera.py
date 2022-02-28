eccd9ef77965cdb3ab9f07f66f45e66b58425d29
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2022-02-27 21:03:25 -0400

    removes unnecessary function

--- facedetection/camera.py
+++ facedetection/camera.py
@@ -1,20 +1,6 @@
 import cv2
 
 
-def open_cam(device, resolution):
-    cam = cv2.VideoCapture(device)
-
-    if not cam.isOpened():
-        raise SystemExit(f"Unable to open {device}")
-
-    width, height = resolution
-    cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    return cam
-
-
 class Camera(cv2.VideoCapture):
     def __init__(self, device, resolution):
         super().__init__(device)
@@ -26,5 +12,3 @@ class Camera(cv2.VideoCapture):
         self.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-
